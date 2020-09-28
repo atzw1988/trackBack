6c52a3e12f6096144e14c2f5d1f485a6206240b9
zhangwei <386553646@qq.com>
2020-09-28 13:46:28 +0800

    页面代码优化

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,4 @@
 const api = {
-  orgId: '40c7055f203749f7a5f6a5f96f43a36e',
-  login: '/park/sys/login/wechatLogin',
-  code: '/park/sys/login/sendMsg',
-  noPayList: '/park/app/parking/order/recoverOrders',
-  rule: '/park/app/parking/check_placeno',
-  checkParking: '/park/app/parking/check_placeno',
-  roadRule: '/park/app/nearby/parking/getChargeRule',
-  parking: '/park/app/parking/order',
-  balance: '/park/sys/user/getBalance',
-  balancePay: '/park/public/h5Pay/balancePay',
-  wxPay: '/park/public/h5Pay/weChatPay',
-  aliPay: '/park/app/h5/alipay',
-  checkCar: '/park/app/car/validCarNo',
   list: '/service-cultivate-soa/api/fbBury/listAll'
 }
 export default api
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,6 +2,7 @@
     <div></div>
 </template>
 <script>
+import { Toast } from 'vant'
 export default {
     data () {
         return {}
@@ -9,12 +10,16 @@ export default {
     mounted () {
         const query = this.$route.query
         let pageName = query.pageName
-        const list = ['stateFa', 'stateJn', 'stateJw', 'stateJt', 'stateZz']
-        if (list.indexOf(pageName) > -1) pageName = 'state'
-        this.$router.push({
-            name: pageName,
-            query: query
-        })
+        if (pageName) {
+            const list = ['stateFa', 'stateJn', 'stateJw', 'stateJt', 'stateZz']
+            if (list.indexOf(pageName) > -1) pageName = 'state'
+            this.$router.push({
+                name: pageName,
+                query: query
+            })
+        } else {
+            Toast.fail('缺少参数')
+        }
     }
 }
 </script>
